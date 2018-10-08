649af94c22ced711f695d78e2f0970fb67c23ba5
PIERRE-PC\Pierre <ppma@itu.dk>
2018-10-08 14:45:56 +0200

    Malmo client can now be run as a single evaluation (Needs events for handling mission end etc.)
    Added log4net (being used by SharpNeat).

---
+++ Evolution/MalmoClient.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Research.Malmo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RunMission.Evolution
+{
+    public class MalmoClient
+    {
+        private AgentHost agentHost;
+        private MissionSpec mission;
+        private WorldState worldState;
+        private ClientPool clientPool;
+
+        private bool isWorldCreated = false;
+
+        private List<string> listOfCommmands;
+
+        public MalmoClient()
+        {
+            isWorldCreated = false;
+
+            clientPool = new ClientPool();
+            clientPool.add(new ClientInfo("127.0.0.1", 10000));
+        }
+
+        public void RunMalmo()
+        {
+            agentHost = new AgentHost();
+
+            InitializeMission();
+
+            CreateWorld();
+
+            if (!TryStartMission())
+            {
+                return;
+            }
+
+            ConsoleOutputWhileMissionLoads();
+
+            Console.WriteLine("Mission has started!");
+
+            agentPerform();
+
+            agentHost.Dispose();
+
+            Console.WriteLine("Mission has ended!");
+        }
+
+        private void InitializeMission()
+        {
+            string missionXMLpath = "C:\\Users\\Pierre\\Documents\\malmoTestAgentInterface\\";
+            missionXMLpath += "myworld.xml";
+            
+            string rawMissionXML = SaferRead(missionXMLpath);
+
+            try
+            {
+                mission = new MissionSpec(rawMissionXML, true);
+            }
+            catch (Exception ex)
+            {
+                string errorLine = "Fatal error when starting a mission in ProgramMalmo: " + ex.Message;
+                System.Diagnostics.Debug.WriteLine("\nFatal error when starting a mission in ProgramMalmo: " + ex.Message);
+                Environment.Exit(1);
+            }
+
+            mission.setModeToCreative();
+            mission.timeLimitInSeconds(10000);
+        }
+
+        private bool TryStartMission()
+        {
+            bool returnValue = true;
+            try
+            {
+                agentHost.startMission(mission, clientPool, new MissionRecordSpec(), 0, "Test Builder");
+            }
+            catch (Exception ex)
+            {
+                string errorLine = "Fatal error when starting a mission in ProgramMalmo: " + ex.Message;
+                Console.WriteLine("Error starting mission: {0}", ex.Message);
+                Environment.Exit(1);
+            }
+            return returnValue;
+        }
+
+        private string SaferRead(string path)
+        {
+            string newFile = null;
+            using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    newFile = reader.ReadToEnd();
+                }
+            }
+            return newFile;
+        }
+
+        private void CreateWorld()
+        {
+            if (!isWorldCreated)
+            {
+                mission.forceWorldReset();
+                isWorldCreated = true;
+            }
+        }
+
+        private void agentPerform()
+        {
+            listOfCommmands = new List<string>();
+            Thread.Sleep(100);
+
+            while(worldState.is_mission_running)
+            {
+                worldState = agentHost.getWorldState();
+                ExecuteCommands();
+                listOfCommmands = new List<string>();
+            }
+        }
+
+        private void ExecuteCommands()
+        {
+            try
+            {
+                foreach (string command in listOfCommmands)
+                {
+                    agentHost.sendCommand(command);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error executing commands at ProgramMalmo: " + ex.Message);
+                string errorLine = "Error executing commands at ProgramMalmo: " + ex.Message;
+            }
+        }
+
+        private void ConsoleOutputWhileMissionLoads()
+        {
+            worldState = agentHost.getWorldState();
+            while (!worldState.has_mission_begun)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+                worldState = agentHost.getWorldState();
+            }
+        }
+    }
+}
--- RunMission.cs
+++ RunMission.cs
@@ -20,8 +20,10 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Xml;
+using log4net.Config;
 using Microsoft.Research.Malmo;
 using Newtonsoft.Json.Linq;
 using RunMission;
@@ -36,142 +38,150 @@ class Program
     const string CHAMPION_FILE = "minecraft_champion.xml";
     public static void Main()
     {
-        // experiment
-        MinecraftBuilderExperiment experiment = new MinecraftBuilderExperiment();
-        // Load config XML.
-        XmlDocument xmlConfig = new XmlDocument();
-        xmlConfig.Load("..\\..\\..\\minecraft.config.xml");
-        experiment.Initialize("Minecraft", xmlConfig.DocumentElement);
+        // XmlConfigurator.Configure(new FileInfo("log4net.properties"));
 
-        _ea = experiment.CreateEvolutionAlgorithm();
-        _ea.UpdateEvent += new EventHandler(ea_UpdateEvent);
-        _ea.StartContinue();
+        // // experiment
+        // MinecraftBuilderExperiment experiment = new MinecraftBuilderExperiment();
+        // // Load config XML.
+        // XmlDocument xmlConfig = new XmlDocument();
+        // xmlConfig.Load("..\\..\\..\\minecraft.config.xml");
+        // experiment.Initialize("Minecraft", xmlConfig.DocumentElement);
 
-        //
+        // _ea = experiment.CreateEvolutionAlgorithm();
+        // _ea.UpdateEvent += new EventHandler(ea_UpdateEvent);
+        // _ea.StartContinue();
 
-        AgentHost agentHost = new AgentHost();
-        AgentHelper agentHelper = new AgentHelper(agentHost);
-        try
-        {
-            var something = Environment.GetCommandLineArgs();
-            Console.WriteLine(something);
-            agentHost.parse( new StringVector( something  ) );
-        }
-        catch( Exception ex )
-        {
-            Console.Error.WriteLine("ERROR: {0}", ex.Message);
-            Console.Error.WriteLine(agentHost.getUsage());
-            Environment.Exit(1);
-        }
-        if( agentHost.receivedArgument("help") )
-        {
-            Console.Error.WriteLine(agentHost.getUsage());
-            Environment.Exit(0);
-        }
-        float startX = -230.5f, startY = 227.5f, startZ = -555.5f;
-        /*MissionSpec mission = new MissionSpec();
-        mission.timeLimitInSeconds(10000);
-        //mission.requestVideo( 320, 240 );
-        //mission.rewardForReachingPosition(19.5f,0.0f,19.5f,100.0f,1.1f);
-        mission.startAt(startX,startY,startZ);
-        */
-        MissionRecordSpec missionRecord = new MissionRecordSpec("./saved_data.tgz");
+        // //
+
+        // AgentHost agentHost = new AgentHost();
+        // AgentHelper agentHelper = new AgentHelper(agentHost);
+        // try
+        // {
+        //     var something = Environment.GetCommandLineArgs();
+        //     Console.WriteLine(something);
+        //     agentHost.parse( new StringVector( something  ) );
+        // }
+        // catch( Exception ex )
+        // {
+        //     Console.Error.WriteLine("ERROR: {0}", ex.Message);
+        //     Console.Error.WriteLine(agentHost.getUsage());
+        //     Environment.Exit(1);
+        // }
+        // if( agentHost.receivedArgument("help") )
+        // {
+        //     Console.Error.WriteLine(agentHost.getUsage());
+        //     Environment.Exit(0);
+        // }
+        // float startX = -230.5f, startY = 227.5f, startZ = -555.5f;
+        // /*MissionSpec mission = new MissionSpec();
+        // mission.timeLimitInSeconds(10000);
+        // //mission.requestVideo( 320, 240 );
+        // //mission.rewardForReachingPosition(19.5f,0.0f,19.5f,100.0f,1.1f);
+        // mission.startAt(startX,startY,startZ);
+        // */
+        // MissionRecordSpec missionRecord = new MissionRecordSpec("./saved_data.tgz");
+
+        // missionRecord.recordCommands();
+        //// missionRecord.recordMP4(20, 400000);
+        // //missionRecord.recordRewards();
+        // missionRecord.recordObservations();
+
+        // //https://microsoft.github.io/malmo/0.14.0/Schemas/Mission.html#element_AgentStart
+        // //https://microsoft.github.io/malmo/0.14.0/Schemas/Mission.html#element_InventoryItem
+
+        // string xml;
+        // if (System.Environment.UserName == "lema") 
+        //     xml = System.IO.File.ReadAllText(@"C:\Users\lema\Documents\GitHub\malmoTestAgentInterface\myworld.xml");
+        // else
+        //     xml = System.IO.File.ReadAllText(@"C:\Users\Pierre\Documents\malmoTestAgentInterface\myworld.xml");
+
+        // MissionSpec mission = new MissionSpec(xml, false);
+        //mission.setModeToCreative();
+        // int attempts = 0;
+        // bool connected = false;
+        // while (!connected)
+        // {
+        //     try
+        //     {
+        //         attempts += 1;
+        //         agentHost.startMission(mission, missionRecord);
+        //         connected = true;
+        //     }
+        //     catch (MissionException ex)
+        //     {
+        //         // Using catch(Exception ex) would also work, but specifying MissionException allows
+        //         // us to access the error code:
+        //         Console.Error.WriteLine("Error starting mission: {0}", ex.Message);
+        //         Console.Error.WriteLine("Error code: {0}", ex.getMissionErrorCode());
+        //         // We can do more specific error handling using this code, eg:
+        //         if (ex.getMissionErrorCode() == MissionException.MissionErrorCode.MISSION_INSUFFICIENT_CLIENTS_AVAILABLE)
+        //             Console.Error.WriteLine("Have you started a Minecraft client?");
+        //         if (attempts >= 3)   // Give up after three goes.
+        //             Environment.Exit(1);
+        //         Thread.Sleep(1000); // Wait a second and try again.
+        //     }
+        // }
+        // WorldState worldState;
+
+        // Console.WriteLine("Waiting for the mission to start");
+        // do
+        // {
+        //     Console.Write(".");
+        //     Thread.Sleep(100);
+        //     worldState = agentHost.getWorldState();
+
+        //     foreach (TimestampedString error in worldState.errors) Console.Error.WriteLine("Error: {0}", error.text);
+        // }
+        // while (!worldState.has_mission_begun);
+
+        // Console.WriteLine();
+
+        // Random rand = new Random();
+        // // main loop:
+        // //agentHost.sendCommand("jump 1");
+
+        // Thread.Sleep(500);
+        // bool goForward = true;
+
+        // bool runonce = false;
+        // do
+        // {
+        //     try
+        //     {
+        //         Thread.Sleep(100);                            
+        //        if (!runonce)
+        //        {
+        //             //agentHelper.PlaceBlock(AgentHelper.Direction.Front);
+        //             //agentHelper.PlaceBlock(AgentHelper.Direction.FrontTop);
+        //             //agentHelper.PlaceBlock(AgentHelper.Direction.Left);
+        //             //agentHelper.CheckSurroundings();
+
+        //             agentHelper.PlaceBlock(AgentHelper.Direction.Front);
+        //             agentHelper.Move(AgentHelper.Direction.Front, true);
+        //             agentHelper.PlaceBlock(AgentHelper.Direction.LeftUnder);
+        //             agentHelper.PlaceBlock(AgentHelper.Direction.Left);
+        //             agentHelper.Move(AgentHelper.Direction.Left, true);
+        //             agentHelper.PlaceBlock(AgentHelper.Direction.Under);
+        //             runonce = true;
+        //        }
+
+        //     }
+        //     catch (ArgumentOutOfRangeException ex)
+        //     {
+        //         Debug.WriteLine("Error reading observations in RunMission");
+        //     }
+        //     foreach (TimestampedReward reward in worldState.rewards) Console.Error.WriteLine("Summed reward: {0}", reward.getValue());
+        //     foreach (TimestampedString error in worldState.errors) Console.Error.WriteLine("Error: {0}", error.text);
+        // }
+        // while (worldState.is_mission_running);
+        // //Console.ReadKey();
+        // Console.WriteLine("Mission has stopped.");
+        // Console.ReadKey();
+
+
+        MalmoClient client = new MalmoClient();
+        client.RunMalmo();
         
-        missionRecord.recordCommands();
-       // missionRecord.recordMP4(20, 400000);
-        //missionRecord.recordRewards();
-        missionRecord.recordObservations();
-
-        //https://microsoft.github.io/malmo/0.14.0/Schemas/Mission.html#element_AgentStart
-        //https://microsoft.github.io/malmo/0.14.0/Schemas/Mission.html#element_InventoryItem
-      
-        string xml;
-        if (System.Environment.UserName == "lema") 
-            xml = System.IO.File.ReadAllText(@"C:\Users\lema\Documents\GitHub\malmoTestAgentInterface\myworld.xml");
-        else
-            xml = System.IO.File.ReadAllText(@"C:\Users\Pierre\Documents\2018-ThesisTest\myworld.xml");
-
-        MissionSpec mission = new MissionSpec(xml, false);
-        mission.setModeToCreative();
-        int attempts = 0;
-        bool connected = false;
-        while (!connected)
-        {
-            try
-            {
-                attempts += 1;
-                agentHost.startMission(mission, missionRecord);
-                connected = true;
-            }
-            catch (MissionException ex)
-            {
-                // Using catch(Exception ex) would also work, but specifying MissionException allows
-                // us to access the error code:
-                Console.Error.WriteLine("Error starting mission: {0}", ex.Message);
-                Console.Error.WriteLine("Error code: {0}", ex.getMissionErrorCode());
-                // We can do more specific error handling using this code, eg:
-                if (ex.getMissionErrorCode() == MissionException.MissionErrorCode.MISSION_INSUFFICIENT_CLIENTS_AVAILABLE)
-                    Console.Error.WriteLine("Have you started a Minecraft client?");
-                if (attempts >= 3)   // Give up after three goes.
-                    Environment.Exit(1);
-                Thread.Sleep(1000); // Wait a second and try again.
-            }
-        }
-        WorldState worldState;
-
-        Console.WriteLine("Waiting for the mission to start");
-        do
-        {
-            Console.Write(".");
-            Thread.Sleep(100);
-            worldState = agentHost.getWorldState();
-
-            foreach (TimestampedString error in worldState.errors) Console.Error.WriteLine("Error: {0}", error.text);
-        }
-        while (!worldState.has_mission_begun);
-        
-        Console.WriteLine();
-
-        Random rand = new Random();
-        // main loop:
-        //agentHost.sendCommand("jump 1");
-
-        Thread.Sleep(500);
-        bool goForward = true;
-
-        bool runonce = false;
-        do
-        {
-            try
-            {
-                Thread.Sleep(100);                            
-               if (!runonce)
-               {
-                    //agentHelper.PlaceBlock(AgentHelper.Direction.Front);
-                    //agentHelper.PlaceBlock(AgentHelper.Direction.FrontTop);
-                    //agentHelper.PlaceBlock(AgentHelper.Direction.Left);
-                    //agentHelper.CheckSurroundings();
-
-                    agentHelper.PlaceBlock(AgentHelper.Direction.Front);
-                    agentHelper.Move(AgentHelper.Direction.Front, true);
-                    agentHelper.PlaceBlock(AgentHelper.Direction.LeftUnder);
-                    agentHelper.PlaceBlock(AgentHelper.Direction.Left);
-                    agentHelper.Move(AgentHelper.Direction.Left, true);
-                    agentHelper.PlaceBlock(AgentHelper.Direction.Under);
-                    runonce = true;
-               }
-
-            }
-            catch (ArgumentOutOfRangeException ex)
-            {
-                Debug.WriteLine("Error reading observations in RunMission");
-            }
-            foreach (TimestampedReward reward in worldState.rewards) Console.Error.WriteLine("Summed reward: {0}", reward.getValue());
-            foreach (TimestampedString error in worldState.errors) Console.Error.WriteLine("Error: {0}", error.text);
-        }
-        while (worldState.is_mission_running);
-        //Console.ReadKey();
-        Console.WriteLine("Mission has stopped.");
         Console.ReadKey();
     }
 
